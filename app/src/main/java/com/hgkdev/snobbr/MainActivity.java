3f66158c1bb548cff6e7bf50c1cd740adb02962e
Hayden Kinney <hkinney2011@gmail.com>
2015-04-16 00:36:24 -0500

    Added API keys and requested permissions

--- app/src/main/java/com/hgkdev/snobbr/MainActivity.java
+++ app/src/main/java/com/hgkdev/snobbr/MainActivity.java
@@ -8,6 +8,9 @@ import android.view.MenuItem;
 
 public class MainActivity extends ActionBarActivity {
 
+    private String untId = this.getString(R.string.untappd_id);
+    private String untSec = this.getString(R.string.untappd_secret);
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
